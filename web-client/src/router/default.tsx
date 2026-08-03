34ddc0cea305a93642e25972de05d41821b3b6e9
HamoodiHajjiri <HamoodiHajjiri@gmail.com>
2026-08-03 18:50:56 +0400

    refactor(router): comment out unused Login and MySpace imports

--- web-client/src/router/default.tsx
+++ web-client/src/router/default.tsx
@@ -5,7 +5,7 @@ import Course from "../pages/course.tsx";
 import CourseLookup from "../pages/course_lookup.tsx";
 import TermsOfService from "../pages/legal/tos.tsx";
 import Privacy from "../pages/legal/privacy.tsx";
-import Login from "../pages/login.tsx";
+// import Login from "../pages/login.tsx";
 import Layout from "../layouts/layout.tsx";
 import Notifications from "../pages/notifications.tsx";
 import ScheduleMaker from "../pages/schedule_maker.tsx";
@@ -13,7 +13,7 @@ import PageNotFound from "../pages/page_not_found.tsx";
 import Feedback from "../pages/feedback.tsx";
 import {ErrorPage} from "../pages/error_page.tsx";
 import RestrictedProfessor from "../pages/restricted_professor.tsx";
-import MySpace from "../pages/my_space.tsx";
+// import MySpace from "../pages/my_space.tsx";
 
 const Router = createBrowserRouter([
     {
